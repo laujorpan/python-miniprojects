d0a36e58f7f707d522d674d7d164280af1e2c413
laujorpan <laujorpan@gmail.com>
2018-01-28 14:01:43 +0100

    Learn about language conventions

--- initial/__init__.py
+++ initial/__init__.py
@@ -1,8 +1,11 @@
 from InitialClass import InitialClass
 
+
 def initial_test():
+    # type: () -> None
     print("Remembering Python knowledge")
-    init_object= InitialClass("primero","otro")
+    init_object = InitialClass("primero", "otro")
     init_object.print_attributes()
+
 
 initial_test()
